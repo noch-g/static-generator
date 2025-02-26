f0bfce989d8c3c06093563326c88bf281de2bf68
noch-g <ladle.doings_0g@icloud.com>
2025-02-26 22:26:43 +0100

    feat: add mypy indentations

--- src/textnode.py
+++ src/textnode.py
@@ -9,13 +9,15 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode:
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text: str, text_type: TextType, url: str=""):
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def __eq__(self, text_node):
-        return self.text == text_node.text and self.text_type == text_node.text_type and self.url == text_node.url
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, TextNode):
+            return NotImplemented
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
