8629f0af6c512c9e2c693271e572e602250d1fe1
noch-g <ladle.doings_0g@icloud.com>
2025-02-27 00:13:49 +0100

    feat: add textnode to leafnode conversion

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -32,6 +32,34 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
+
+    def test_text_node_to_html_node(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.to_html(), "<b>This is bold</b>")
+
+        node = TextNode("This is italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.to_html(), "<i>This is italic</i>")
+
+        node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.to_html(), "<code>This is code</code>")
+
+        node = TextNode("Click here", TextType.LINK, "www.example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.to_html(), '<a href="www.example.com">Click here</a>')
+
+        node = TextNode("This is an image", TextType.IMAGE, "www.example.com/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.to_html(), '<img src="www.example.com/image.png" alt="This is an image"></img>')
+
+
 if __name__ == "__main__":
     unittest.main()
 
