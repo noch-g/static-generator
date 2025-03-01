64a261c51444f869487a7d1f639c98583fbedd92
noch-g <ladle.doings_0g@icloud.com>
2025-03-01 15:10:26 +0100

    feat: add files copy from static to public

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
 from textnode import TextNode, TextType
 
+import os
+import shutil
+
+def copy_r(folder_src: str, folder_dest: str) -> None:
+    if not os.path.exists(folder_dest):
+        os.mkdir(folder_dest)
+    for item in os.listdir(folder_src):
+        if os.path.isfile(os.path.join(folder_src, item)):
+            shutil.copy(os.path.join(folder_src, item), os.path.join(folder_dest, item))
+        else:
+            copy_r(os.path.join(folder_src, item), os.path.join(folder_dest, item))
+
 if __name__ == "__main__":
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    copy_r("static", "public")
